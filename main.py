ad9dba20e67d6f5fceb27e472e0ac42c5ac2074c
Alex <alexromantsov@gmail.com>
2017-11-16 14:05:27 +1000

    Update OSError
    The change to exception. Added parameter fail_c, when the trigger of the exception three times error is sent to slack.

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time, urllib, socket
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+import time
+import urllib
+import urllib.request
+
 from slacker import Slacker
 
 slack = Slacker('TOKEN-SLACK-BOT')
 
-TIMERSLEEP = 15
-PAUSE = 60
+TIMERSLEEP = 5
+PAUSE = 20
 
 site_pages = [
-'https://google.com',
-'https://yandex.ru'
+'https://yandex.ru',
+'https://google.com'
 ]
 
-failed_pages = [];
+failed_pages = []
 
-def check_pages (pages):
-	while True:
-		try:
-			for page_url in pages:
-				code = urllib.request.urlopen(page_url, timeout=15).getcode()
-				time.sleep(TIMERSLEEP)
-				t_error = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
-				if (code not in [200, 301]):
-					failed_pages.append(page_url)
-					slack.chat.post_message('#checkpages', 'Error: ' + t_error + "{0} - {1}".format(page_url, code))
-		except socket.error as e:
-			t_error = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
-			slack.chat.post_message('#checkpages', 'Error socket: ' + t_error)
-			time.sleep(PAUSE)
+def check_pages(pages):
+    fail_c = int(0)
+
+    while True:
+        try:
+            for page_url in pages:
+                code = urllib.request.urlopen(page_url, timeout=15).getcode()
+                t_error = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
+                if (code not in [200, 301]):
+                    failed_pages.append(page_url)
+		    slack.chat.post_message('#checkpages', 'Error: ' + t_error + "{0} - {1}".format(page_url, code))
+        except OSError as e:
+            t_error = time.strftime("%a, %d %b %Y %H:%M:%S +0000 ", time.gmtime())
+            fail_c += 1
+            if fail_c == 3:
+		slack.chat.post_message('#checkpages', 'Error socket: ' + t_error  + str(e))
+		fail_c = 0
+            time.sleep(PAUSE)
 
 check_pages(site_pages)
